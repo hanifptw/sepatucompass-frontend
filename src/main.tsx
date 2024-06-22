bf5348e033a995c113b27c09cc57335b7c84e5dc
Brilliant Hanif Almubarak <brillianthanif@gmail.com>
2024-06-22 13:13:52 +0700

    build with hono + typescript + react router

---
+++ src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { RootRoute, loader as rootLoader } from "./routes/root";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootRoute />,
+    loader: rootLoader,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
