0a51d7684ad4f33a2156a7ad49fbf724e8880b53
Brilliant Hanif Almubarak <brillianthanif@gmail.com>
2024-07-01 20:58:50 +0700

    fix: refine data types from backend

---
+++ src/routes/types/index.ts
@@ -0,0 +1,40 @@
+export type Order = {
+  id: string;
+  userId: string | null;
+  status: string;
+  
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type Product = {
+  id: string;
+  slug: string;
+  name: string;
+  imageURL: string;
+  price: number;
+  description: string;
+  sizes: string[];
+
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type user = {
+  id: string;
+  username: string;
+  email: string;
+
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+export type OrderItem = {
+    id: string;
+    productId: string;
+    quantity: number;
+    orderId: string | null;
+
+    createdAt: Date;
+    updatedAt: Date;
+}
